7fa27d6c25a85dbe5519aee115d9b38bff83e0be
Sabrina Fernández <sabrina.fer.zam@gmail.com>
2017-12-05 16:22:30 +0100

    Fix

--- src/main/java/webSemantica/app/AppApplication.java
+++ src/main/java/webSemantica/app/AppApplication.java
@@ -2,11 +2,18 @@ package webSemantica.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AppApplication {
+public class AppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
+	
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	      return builder.sources(AppApplication.class);
+	  }
 }
